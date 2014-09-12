65e6dd73025eb2376aa44bd9fc86b90263a8a2dc
LeeHey <lcxfs1991@gmail.com>
2014-09-12 21:24:36 +0800

    change question detail
    change question detail

--- data.php
+++ data.php
@@ -2,45 +2,48 @@
 
 header('Content-Type: text/html; charset=utf-8');
 
-$data[0] = ['question' => '请回答', 'point' => array('今年的年份是?')];
-$data[1] = ['question' => '请回答', 'point' => array('现在是什么季节?')];
-$data[2] = ['question' => '请回答', 'point' => array('今天是几号?')];
-$data[3] = ['question' => '请回答', 'point' => array('今天是星期几?')];
-$data[4] = ['question' => '请回答', 'point' => array('现在是几月份?')];
-$data[5] = ['question' => '请回答', 'point' => array('你能告诉我现在我们在哪里(省、市)?')];
-$data[6] = ['question' => '请回答', 'point' => array('你住在哪个区县?')];
-$data[7] = ['question' => '请回答', 'point' => array('你住在什么街道?')];
-$data[8] = ['question' => '请回答', 'point' => array('我们现在是在几楼?')];
-$data[9] = ['question' => '请回答', 'point' => array('这里是什么地方?')];
-$data[10] = ['question' => '说出三样东西的名称(每样东西一秒钟), 继续重复这三样东西,直到受试者能全部正确复述,最多可重复6次,还不能复述记0分.', 
-			 'point' => array('皮球', '国旗', '树木')];
+$data[0] = ['question' => '1. 今年的年份是?', 'point' => array('')];
+$data[1] = ['question' => '2. 现在是什么季节?', 'point' => array('')];
+$data[2] = ['question' => '3. 今天是几号?', 'point' => array('')];
+$data[3] = ['question' => '4. 今天是星期几?', 'point' => array('')];
+$data[4] = ['question' => '5. 现在是几月份?', 'point' => array('')];
+$data[5] = ['question' => '6. 你现在在哪个省哪个市?', 'point' => array('')];
+$data[6] = ['question' => '7. 你住在哪个区县?', 'point' => array('')];
+$data[7] = ['question' => '8. 你住在什么街道(村)?', 'point' => array('')];
+$data[8] = ['question' => '9. 你现在是在几楼?', 'point' => array('')];
+$data[9] = ['question' => '10. 这里是什么地方?', 'point' => array('')];
+$data[10] = ['question' => '11. 现在, 我说三样东西的名称, 请你复述并记住。', 'instruction' => array('提示: 测试者每正确复述一个词, 计1分, 考官最多可重复说六次'), 
+			 'point' => array('皮球 (1分)', '国旗 (1分)', '树木 (1分)')];
 
-$data[11] = ['question' => '做100减7,然后从所得到的数目再减去7', 
-			 'point' => array('100 - 7 = ?', '93 - 7 = ? ', '86 - 7 = ? ', '79 - 7 = ? ', '72 - 7 = ? ', '65 - 7 = ? ')];
+$data[11] = ['question' => '12. 做100减7,然后从所得到的数目再减去7, 如此一直计算下去，把每一个答案说出来，直到我说停为止。', 'type' => 'question',
+			 'instruction'=> array('提示: 如果算错, 也接着减下去, 每次算对得一分。可参考下一页的得分点。'), 'point' => array()];
 
-$data[12] = ['question' => '现在告诉我刚才我要你记住的三样东西是什么?', 
-			 'point' => array('皮球', '国旗', '树木')];	
+$data[12] = ['question' => '以下是得分点。', 
+			 'point' => array('100 - 7 = ? (1分)', '93  - 7 = ? (1分)', '86  - 7 = ? (1分)', '79  - 7 = ? (1分)', '72  - 7 = ? (1分)')];
 
-$data[13] = ['question' => '请问这是什么?', 'point' => array('橡皮'), 'img' => array('res/eraser.png')];	
+$data[13] = ['question' => '13. 现在请说出刚才让你记住的三样东西是什么?', 
+			 'point' => array('皮球 (1分)', '国旗 (1分)', '树木 (1分)')];	
 
-$data[14] = ['question' => '请问这是什么?', 'point' => array('铅笔'), 'img' => array('res/pen.png')];	
+$data[14] = ['question' => '14. 考官出示一张纸问, 这是什么？', 'point' => array('纸')];	
 
-$data[15] = ['question' => '说"四十四只石狮子"一遍,让受试者清楚重复一遍,只有正确咬字清楚才记1分', 
+$data[15] = ['question' => '15. 考官出示一支笔问，这是什么?', 'point' => array('笔')];	
+
+$data[16] = ['question' => '16. 现在我说一句话，请你清楚地重复一遍，这句话是"四十四只狮子"', 'instruction' => array('提示: 考官只说一遍，被测者说正确才记1分'), 
 			 'point' => array('四十四只石狮子')];	
 
-$data[16] = ['question' => '把手机交给测试者,让他按照屏幕内容去做以下动作:', 
+$data[17] = ['question' => '17. 把手机交给测试者,让他按照屏幕内容去做以下动作:', 
 			 'point' => array('请闭上你的眼睛')];	
 
 
-$data[17] = ['question' => '给受试者一张纸,并说"请你用右手拿这张纸,再请你用双手把它对折,然后将纸放在大腿上"(不重复说明,也不示范)', 
-			 'point' => array('用右手拿纸', '将纸对折', '放在大腿上')];	
+$data[18] = ['question' => '18. 给受试者一张纸,并说"请你用右手拿这张纸,再请你用双手把它对折,然后将纸放在大腿上"。', 'insruction' => '提示: 考官不重复, 也不示范', 
+			 'point' => array('用右手拿纸 (1分)', '将纸对折 (1分)', '放在大腿上 (1分)')];	
 
-$data[18] = ['question' => '请你说一句完整的句子。句子必须有主词、动词,有意义', 
-			 'point' => array('让测试者记下所叙述的句子的全文,并写出来')];	
+$data[19] = ['question' => '19. 请你说一句完整的、有意义的句子', 'instruction' => array('提示: 句子必须有主语、动词'), 
+			 'point' => array('句子符合有主语、动词和有意义的句子')];	
 
-$data[19] = ['question' => '这是一张图,请你在同一张纸上照样把它画出来', 'point' => array('卡片上有两个5边形叠加的图片'), 'img' => array('res/card.jpg')];	
+$data[20] = ['question' => '20.请在纸上画出这张图。', 'type' => 'question', 'point' => array(''), 'img' => array('res/card.jpg')];	
 
-
+$data[21] = ['question' => '两个五边形图案，交叉处形成一个小四边形，才算对。', 'point' => array('')];	
 
 $json_data = json_encode($data, JSON_UNESCAPED_UNICODE);
 
--- questionController.js
+++ questionController.js
@@ -1,15 +1,18 @@
 function questionController($scope, $http){
 	
-	$scope.totalScore = 0;
-	$scope.study = 0;
-	$scope.studyCheck = true;
-	$scope.studyHint = "";
-	$scope.questionStart = false;
-	$scope.prevScore = [];
-	$scope.questionResult = false;
-	$scope.ResultText = "";
-	$scope.figure = null;
+	$scope.totalScore = 0;      		// total score of the test
+	$scope.study = 0;					// education level
+	$scope.studyCheck = true;			// education level block display true/false
+	$scope.studyHint = "";				// hint of education level checking
+	$scope.questionStart = false;		// question block display true/false
+	$scope.prevScore = [];				// store previous stock score in order to go back and deduct from total score
+	$scope.questionResult = false;		// question result block display true/false
+	$scope.ResultText = "";				// question result text
+	$scope.figure = null;				// image of questions
+	$scope.hint = null;					// question instruction
+	$scope.nextPageDisplay = false;		// question next button display true/false
 
+	// get education level
 	$scope.getStudy = function(){
 		if ($scope.study > 0){
 			$scope.studyCheck = false;
@@ -20,12 +23,14 @@ function questionController($scope, $http){
 		}
 	}
 
+	//get question data
 	$http.get('data.php').success(function(response){
 
 		var i = 0;
 		var data = response;
 		$scope.showQuestion = data[i]; 
 
+		// generate score button
 		$scope.btn = function(){
 			$scope.pointsLen = [];
 
@@ -36,20 +41,35 @@ function questionController($scope, $http){
 		
 		$scope.btn();
 
+		// score button function
 		$scope.page = function(score){
 
 			$scope.prevScore.push(score);
 			$scope.totalScore += $scope.prevScore[i];
 			$scope.figure = null;
+			$scope.hint = null;
 
 			if (data[i + 1] != undefined){
 				$scope.showQuestion = data[++i];
 
-				if ($scope.showQuestion['img'] != undefined){
-					$scope.figure = $scope.showQuestion['img'][0];
-				}
+				$scope.getContent();
 
-				$scope.btn();
+				// if ($scope.showQuestion['img'] != undefined){
+				// 	$scope.figure = $scope.showQuestion['img'][0];
+				// }
+
+				// if ($scope.showQuestion['instruction'] != undefined){
+				// 	$scope.hint = $scope.showQuestion['instruction'][0];
+				// }
+
+				// if ($scope.showQuestion['type'] != undefined){
+				// 	$scope.nextPageDisplay = true;
+				// 	$scope.pointsLen = [];
+				// }
+				// else {
+				// 	$scope.btn();
+				// 	$scope.nextPageDisplay = false;
+				// }
 			}
 			else if (data[i + 1] == undefined){
 
@@ -60,13 +80,18 @@ function questionController($scope, $http){
 			
 		}
 
+		//go back button function
 		$scope.goBack = function(){
 
 			if (i > 0){
 				$scope.totalScore -= $scope.prevScore[i - 1];
 				$scope.prevScore.pop();
 				$scope.showQuestion = data[--i];
-				$scope.btn();
+				$scope.figure = null;
+				$scope.hint = null;
+
+				$scope.getContent();
+				
 			}
 			else if (i == 0){
 				$scope.studyCheck = true;
@@ -75,11 +100,33 @@ function questionController($scope, $http){
 			
 		}
 
+		//reusage part of score button and go back button
+		$scope.getContent = function(){
+
+			if ($scope.showQuestion['img'] != undefined){
+					$scope.figure = $scope.showQuestion['img'][0];
+			}
+
+			if ($scope.showQuestion['hint'] != undefined){
+					$scope.hint = $scope.showQuestion['instruction'][0];
+			}
+
+			if ($scope.showQuestion['type'] != undefined){
+				$scope.nextPageDisplay = true;
+				$scope.pointsLen = [];
+			}
+			else {
+				$scope.btn();
+				$scope.nextPageDisplay = false;
+			}
+		}
+
+		//display question result
 		$scope.getResult = function(){
 			
-			$scope.ResultText = "被测试者的得分是"+$scope.totalScore;
-			var risk = ". 患痴呆症的风险较高, 请尽早到医院进行全面检查.";
-			var safe = ". 患痴呆症的风险较小, 但亦请密切留意老人健康.";
+			$scope.ResultText = "恭喜你完成测试, 总分为"+$scope.totalScore;
+			var risk = "。 您的智能状态受损, 建议到正规医院诊断, 请关注\"不痴呆BCD\"公众号, 抓住黄金干预期。";
+			var safe = "。 您的智能状态良好，建议每半年测试一次, 请关注\"不痴呆BCD\"公众号, 爱护脑健康。";
 
 			if ($scope.study == 1){
 
